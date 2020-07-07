faa731cb442d8e2172c2a2d8815ddd8cf940a38e
Paul Opiyo <59094545+paulopiyo777@users.noreply.github.com>
2020-07-07 01:12:36 -0500

    add dashboard

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import * as React from "react";
 import { Admin, Resource } from "react-admin";
 import { UserList } from "./users";
 import { PostList, PostEdit, PostCreate } from "./posts";
+import Dashboard from "./Dashboard";
 import jsonServerProvider from "ra-data-json-server";
+import PostIcon from "@material-ui/icons/Book";
+import UserIcon from "@material-ui/icons/Group";
 
 // fetch data from the REST API
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
     <Resource
       name='posts'
       list={PostList}
       edit={PostEdit}
       create={PostCreate}
+      icon={PostIcon}
     />
-    <Resource name='users' list={UserList} />
+    <Resource name='users' list={UserList} icon={UserIcon} />
   </Admin>
 );
 
