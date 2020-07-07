4b3dd80b651d099ae22ddc8d8a79d1353b1299aa
Paul Opiyo <59094545+paulopiyo777@users.noreply.github.com>
2020-07-07 00:57:34 -0500

    add creation and editing capabilities

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@
 import * as React from "react";
 import { Admin, Resource } from "react-admin";
 import { UserList } from "./users";
+import { PostList, PostEdit, PostCreate } from "./posts";
 import jsonServerProvider from "ra-data-json-server";
 
 // fetch data from the REST API
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 const App = () => (
   <Admin dataProvider={dataProvider}>
+    <Resource
+      name='posts'
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+    />
     <Resource name='users' list={UserList} />
   </Admin>
 );
---
+++ src/posts.js
@@ -0,0 +1,55 @@
+import * as React from "react";
+import {
+  List,
+  Datagrid,
+  TextField,
+  ReferenceField,
+  EditButton,
+  Edit,
+  SimpleForm,
+  TextInput,
+  ReferenceInput,
+  SelectInput,
+  Create,
+} from "react-admin";
+
+// render user posts as a list
+export const PostList = (props) => (
+  <List {...props}>
+    <Datagrid>
+      <TextField source='id' />
+      <ReferenceField source='userId' reference='users'>
+        <TextField source='name' />
+      </ReferenceField>
+      <TextField source='title' />
+      <EditButton />
+    </Datagrid>
+  </List>
+);
+
+// functionality to edit posts
+export const PostEdit = (props) => (
+  <Edit {...props}>
+    <SimpleForm>
+      <TextInput disabled source='id' />
+      <ReferenceInput source='userId' reference='users'>
+        <SelectInput optionText='name' />
+      </ReferenceInput>
+      <TextInput source='title' />
+      <TextInput multiline source='body' />
+    </SimpleForm>
+  </Edit>
+);
+
+// functionality to create posts
+export const PostCreate = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <ReferenceInput source='userId' reference='users'>
+        <SelectInput optionText='name' />
+      </ReferenceInput>
+      <TextInput source='title' />
+      <TextInput multiline source='body' />
+    </SimpleForm>
+  </Create>
+);
